fff7a266fc9e28fabf6e37ef9199b74c48994af0
Eamonn Upperton <uppertoe@gmail.com>
2023-05-17 14:54:48 +1000

    initial

---
+++ spanza_journal_watch/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from spanza_journal_watch.users.models import User
+from spanza_journal_watch.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
