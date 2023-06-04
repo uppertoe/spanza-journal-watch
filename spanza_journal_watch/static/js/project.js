919ec6c97c912b9e5c1a32927201c2947e7e47f7
Eamonn Upperton <uppertoe@gmail.com>
2023-06-04 22:01:29 +1000

    install htmx

--- spanza_journal_watch/static/js/project.js
+++ spanza_journal_watch/static/js/project.js
@@ -1,3 +1,4 @@
 import '../sass/project.scss';
 
 /* Project specific Javascript goes here. */
+import '../js/htmx.js';
--- spanza_journal_watch/static/js/vendors.js
+++ spanza_journal_watch/static/js/vendors.js
@@ -1,2 +1,3 @@
 import '@popperjs/core';
 import 'bootstrap';
+import 'htmx.org';
