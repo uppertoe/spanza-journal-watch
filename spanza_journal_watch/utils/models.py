b061ac0cb0e7184803be026f7b1a24a639e42417
Eamonn Upperton <uppertoe@gmail.com>
2023-06-20 00:25:52 +1000

    start new migration

--- spanza_journal_watch/utils/models.py
+++ spanza_journal_watch/utils/models.py
@@ -1,4 +1,4 @@
-from django.contrib.postgres.search import SearchRank, SearchVector
+from django.contrib.postgres.search import SearchRank, SearchVector, TrigramSimilarity
 from django.db import models
 
 
@@ -36,12 +36,13 @@ class ModelSearchMixin:
         return search_vector
 
     @classmethod
-    def search(cls, search_query, sort=True, rank=0.3):
+    def search(cls, search_query, sort=True, rank=0.3, similarity=0.3):
         search_vector = cls.get_search_vector()
         search_results = (
             cls.objects.exclude(active=False)
+            .annotate(similarity=TrigramSimilarity(*cls.search_fields, search_query))
             .annotate(rank=SearchRank(search_vector, search_query))
-            .filter(rank__gte=rank)
+            .filter(rank__gte=rank, similarity__gte=similarity)
         )
         if sort:
             search_results = search_results.order_by(models.F("rank").desc())
