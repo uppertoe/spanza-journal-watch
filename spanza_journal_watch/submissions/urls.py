13b1e9d00f3740a1f7607570ce06951a1b53b822
Eamonn Upperton <uppertoe@gmail.com>
2023-06-20 21:51:03 +1000

    implement active class for nav link

--- spanza_journal_watch/submissions/urls.py
+++ spanza_journal_watch/submissions/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 app_name = "submissions"
 urlpatterns = [
-    # TODO redirect reviews/ to a searchview
     path("reviews", RedirectView.as_view(url="search", permanent=False), name="review_list"),  # Redirect to search
     path("reviews/<slug:slug>", views.ReviewDetailView.as_view(), name="review_detail"),
     path("issues", views.IssueListView.as_view(), name="issue_list"),
@@ -14,5 +13,5 @@ urlpatterns = [
     path("tags", views.TagListView.as_view(), name="tag_list"),
     path("tags/<slug:slug>", views.TagDetailView.as_view(), name="tag_detail"),
     path("search", views.SearchView.as_view(), name="search"),
-    path("ajax/tags", views.ajax_get_tags, name="ajax_get_tags"),
+    path("ajax/tags", views.ajax_get_tags, name="ajax_get_tags"),  # Hardcoded in base.html
 ]
