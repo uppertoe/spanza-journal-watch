49f9b321cd3cd4246ba6da628601608fb25c1065
Eamonn Upperton <uppertoe@gmail.com>
2023-08-22 01:06:13 +0900

    fix allowed_hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,7 +14,7 @@ from .base import LOG_FILE, env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["journalwatch.org.au"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=[".journalwatch.org.au"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
