c8f29592b289839ecab22867234a1a028af8235e
Eamonn Upperton <uppertoe@gmail.com>
2023-08-23 18:36:21 +0900

    add retry to subscribe email

--- spanza_journal_watch/newsletter/tasks.py
+++ spanza_journal_watch/newsletter/tasks.py
@@ -17,16 +17,19 @@ def send_newsletter(newsletter_pk):
     print(f"{successful} of {len(messages)} emails sent successfully")
 
 
-@celery_app.task()
-def send_confirmation_email(subscriber_pk):
+@celery_app.task(bind=True, max_retries=3)
+def send_confirmation_email(self, subscriber_pk):
     """Sends a single EmailMessage object"""
 
     from .models import Subscriber  # Avoid circular import
 
-    subscriber = Subscriber.objects.get(pk=subscriber_pk)
-    email = subscriber.generate_confirmation_email()
-    email.send()
-    print(f"Sign-up email sent to {subscriber.email}")
+    try:
+        subscriber = Subscriber.objects.get(pk=subscriber_pk)
+        email = subscriber.generate_confirmation_email()
+        email.send()
+        print(f"Sign-up email sent to {subscriber.email}")
+    except Subscriber.DoesNotExist as exc:
+        raise self.retry(exc=exc, countdown=3 * 60)
 
 
 @celery_app.task()
