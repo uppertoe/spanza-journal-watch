50ebd2e3237ba3bbb608374eb6f81c543db5302c
Eamonn Upperton <uppertoe@gmail.com>
2023-08-25 19:32:28 +0900

    add email media models

--- spanza_journal_watch/newsletter/models.py
+++ spanza_journal_watch/newsletter/models.py
@@ -9,8 +9,73 @@ from django.template.loader import render_to_string
 from django.urls import reverse
 
 from spanza_journal_watch.submissions.models import Issue
+from spanza_journal_watch.utils.modelmethods import name_font, name_image
 
-# from .tasks import send_newsletter
+
+class EmailFont(models.Model):
+    TITLE = "TI"
+    BODY = "BO"
+    OTHER = "OT"
+    TYPE_CHOICES = [
+        (TITLE, "Title"),
+        (BODY, "BODY"),
+        (OTHER, "Other"),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    type = models.CharField(max_length=2, choices=TYPE_CHOICES, default=OTHER)
+    font = models.FileField(
+        upload_to=name_font,
+        blank=True,
+        null=True,
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    @classmethod
+    def get_latest_title(cls):
+        return cls.filter(type=cls.TITLE).order_by("-modified").first()
+
+    @classmethod
+    def get_latest_body(cls):
+        return cls.filter(type=cls.BODY).order_by("-modified").first()
+
+    def __str__(self):
+        return self.name
+
+
+class EmailImage(models.Model):
+    HEADER = "HE"
+    LOGO = "LO"
+    OTHER = "OT"
+    TYPE_CHOICES = [
+        (HEADER, "Header"),
+        (LOGO, "Logo"),
+        (OTHER, "Other"),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    type = models.CharField(max_length=2, choices=TYPE_CHOICES, default=OTHER)
+    image = models.ImageField(
+        upload_to=name_image,
+        blank=True,
+        null=True,
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    @classmethod
+    def get_latest_header(cls):
+        return cls.filter(type=cls.HEADER).order_by("-modified").first()
+
+    @classmethod
+    def get_latest_logo(cls):
+        return cls.filter(type=cls.LOGO).order_by("-modified").first()
+
+    def __str__(self):
+        return self.name
 
 
 class Subscriber(models.Model):
@@ -21,26 +86,18 @@ class Subscriber(models.Model):
     unsubscribe_token = models.CharField(max_length=64, blank=True, null=True)
 
     def generate_confirmation_email_html(self):
-        template_name = "newsletter/email_confirmation.html"
+        template = "newsletter/email_confirmation.html"
         context = {"subscriber": self}
-        return render_to_string(template_name, context)
+        return render_to_string(template, context)
+
+    def generate_confirmation_email_txt(self):
+        template = "newsletter/email_confirmation.txt"
+        context = {"subscriber": self}
+        return render_to_string(template, context)
 
     def generate_confirmation_email(self):
-        body = f"""
-        Thank you for subscribing to SPANZA Journal Watch.
+        body = self.generate_confirmation_email_txt()
 
-        You will receive updates with each issue of Journal Watch,
-        which is published every two months.
-
-        You are receiving this email because you subscribed at
-        journalwatch.org.au using the email {self.email}
-
-        You can unsubscribe at any time by visiting the following address:
-        {self.get_unsubscribe_link()}
-
-        Sincerely,
-        The SPANZA Journal Watch team
-        """
         email = mail.EmailMultiAlternatives(
             subject="Journal Watch Subscription",
             body=body,
@@ -81,14 +138,25 @@ class Newsletter(models.Model):
     is_test_sent = models.BooleanField(default=False)
     issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
 
-    def generate_html_content(self, subscriber):
-        template_name = "newsletter/email_newsletter.html"
+    # Email media
+    title_font = models.ForeignKey(EmailFont, on_delete=models.CASCADE, default=EmailFont.get_latest_title)
+    header_image = models.ForeignKey(EmailImage, on_delete=models.CASCADE, default=EmailImage.get_latest_header)
+
+    def render_email(self, subscriber, template):
         context = {
             "newsletter": self,
             "issue": self.issue,
             "subscriber": subscriber,
         }
-        return render_to_string(template_name, context)
+        return render_to_string(template, context)
+
+    def generate_html_content(self, subscriber):
+        template = "newsletter/email_newsletter.html"
+        return self.render_email(subscriber, template)
+
+    def generate_txt_content(self, subscriber):
+        template = "newsletter/email_newsletter.txt"
+        return self.render_email(subscriber, template)
 
     def generate_emails(self):
         subscribers = Subscriber.objects.exclude(subscribed=False)
@@ -96,7 +164,7 @@ class Newsletter(models.Model):
         for subscriber in subscribers:
             email = mail.EmailMultiAlternatives(
                 subject=self.subject,
-                body=self.content,
+                body=self.generate_txt_content(subscriber),
                 to=[subscriber.email],
             )
             email.attach_alternative(self.generate_html_content(subscriber), "text/html")
