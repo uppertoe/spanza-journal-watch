b4045bcf94cc6e0349ded120093e257d9443eee8
Eamonn Upperton <uppertoe@gmail.com>
2023-08-30 23:28:04 +1000

    implement bounced and complaint handling

--- spanza_journal_watch/newsletter/signals.py
+++ spanza_journal_watch/newsletter/signals.py
@@ -1,18 +1,34 @@
 from django.conf import settings
 
+from .models import Subscriber
+
 if not settings.DEBUG:  # Anymail only available in production
     from anymail.signals import tracking
     from django.dispatch import receiver
 
     @receiver(tracking)  # add weak=False if inside some other function/class
     def handle_tracking(sender, event, esp_name, **kwargs):
-        if esp_name == "Amazon SES":
+        def _get_subscriber(email):
             try:
-                message_tags = {name: values[0] for name, values in event.esp_event["mail"]["tags"].items()}
-            except KeyError:
-                message_tags = None  # SES Notification (not Event Publishing) event
-            print(
-                "Message {} to {} event {}: Message Tags {!r}".format(
-                    event.message_id, event.recipient, event.event_type, message_tags
-                )
-            )
+                subscriber = Subscriber.objects.get(email=email)
+            except Subscriber.DoesNotExist:
+                subscriber = None
+            return subscriber
+
+        if event.event_type == "bounced":
+            subscriber = _get_subscriber(event.recipient)
+            if subscriber:
+                subscriber.bounced = True
+                subscriber.save()
+                print(f"Email to {subscriber} bounced; removed from mailing list")
+            else:
+                print(f"No subscriber found for bounced email: {subscriber}")
+
+        if event.event_type == "complained":
+            subscriber = _get_subscriber(event.recipient)
+            if subscriber:
+                subscriber.complained = True
+                subscriber.save()
+                print(f"Email to {subscriber} complained; removed from mailing list")
+            else:
+                print(f"No subscriber found for complaint: {subscriber}")
