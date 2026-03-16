8299f01a45cb2adaf113457540c933957c6f72fc
Eamonn Upperton <uppertoe@gmail.com>
2026-03-16 13:52:01 +1100

    fix dev memory issues

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -3,7 +3,18 @@ const commonConfig = require('./common.config');
 
 module.exports = merge(commonConfig, {
   mode: 'development',
-  devtool: 'inline-source-map',
+  // Use a lower-memory source map mode for Docker-based local development.
+  devtool: 'eval-cheap-module-source-map',
+  watchOptions: {
+    ignored: [
+      '**/.git/**',
+      '**/.venv/**',
+      '**/backups/**',
+      '**/docs/_build/**',
+      '**/tests/regression/snapshots/**',
+      '**/spanza_journal_watch/media/**',
+    ],
+  },
   devServer: {
     client: {
       overlay: {
@@ -13,9 +24,12 @@ module.exports = merge(commonConfig, {
       },
     },
     port: 3000,
-    proxy: {
-      '/': 'http://django:8000',
-    },
+    proxy: [
+      {
+        context: () => true,
+        target: 'http://django:8000',
+      },
+    ],
     // We need hot=false (Disable HMR) to set liveReload=true
     hot: false,
     liveReload: true,
